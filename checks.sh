e18b138ebad7ad98f666db09fef413f7bcda51b1
RadBoris <rad.borislavov@gmail.com>
2019-06-02 15:45:16 +0000

    update shell

--- checks.sh
+++ checks.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-if [ $# != 2 ] ; then
-    echo "usage $0 PATH_TO_BINARY TARGET_FOLDER"
-    exit 1
-fi
+croot="new-root"
+cur_bash="/bin/bash"
 
-PATH_TO_BINARY="$1"
-TARGET_FOLDER="$2"
+if [ ! -d "$PWD/new-root" ] ; then
+    echo "Creating target dir..."
+    mkdir $PWD/new-root
 
 
-if [ ! -f "$PATH_TO_BINARY" ] ; then
-    echo "The file '$PATH_TO_BINARY' was not found. Aborting!"
-    exit 1
-fi
+cp --parents -v "$cur_bash" "$PWD/new-root"
 
-cp --parents -v "$PATH_TO_BINARY" "$TARGET_FOLDER"
-
-for lib in `ldd "$PATH_TO_BINARY" | cut -d'>' -f2 | awk '{print $1}'` ; do
+for lib in `ldd "$cur_bash" | cut -d'>' -f2 | awk '{print $1}'` ; do
    if [ -f "$lib" ] ; then
-        cp -v --parents "$lib" "$TARGET_FOLDER"
-   fi  
+        cp -v --parents "$lib" "$PWD/new-root"
+   fi
 done
 
-if [ ! -d "$TARGET_FOLDER/lib64" ] ; then
-    mkdir -v "$TARGET_FOLDER/lib64"
+if [ ! -d "$PWD/new-root/lib64" ] ; then
+    mkdir -v "$PWD/new-root/lib64"
 fi
 
+echo "Done copying bash dependencies"
 
+chroot $PWD/new-root /bin/bash
+
+echo "In new root"
+
+fi
+
+chroot $PWD/new-root /bin/bash
+
+echo "In new root. Jail dir existed"
